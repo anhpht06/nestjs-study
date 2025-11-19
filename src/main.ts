d113e22f059a185fb8bc95c2b91bdf7852908c75
Andy <anhpht@iceteasoftware.com>
2025-11-19 14:28:06 +0700

    feat: Setup API documentation using Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import chalk from 'chalk';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
+  const globalPrefix = 'api/v1';
+  const globalSwaggerPrefix = 'api/docs';
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix(globalPrefix);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Study API')
+    .setDescription('The API documentation for NestJS study project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalSwaggerPrefix, app, document);
+
+  await app.listen(port);
+
+  console.log(
+    chalk.blue('🔗 API: ') +
+      chalk.bold(`http://localhost:3000/${globalPrefix}`),
+  );
+  console.log(
+    chalk.blue('📚 Swagger: ') +
+      chalk.bold(`http://localhost:3000/${globalSwaggerPrefix}`),
+  );
 }
 bootstrap();
